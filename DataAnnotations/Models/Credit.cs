47410d9e71fb763554798172cf33af85d10bc90b
SahilKumar <sahilnarwal1032@gmail.com>
2022-04-16 09:12:53 -0500

    lab 2

---
+++ DataAnnotations/Models/Credit.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAnnotations.Models
+{
+    public class Credit
+    {
+        
+            [Key]
+            public string CreditId { get; set; }
+
+            public bool IsValid { get; set; }
+            [StringLength(20)]
+            public string HolderName { get; set; }
+
+            public DateTime ExpiryDate { get; set; }
+
+            public string ClientId { get; set; }
+            public Client CLient { get; set; }
+        
+    }
+}
